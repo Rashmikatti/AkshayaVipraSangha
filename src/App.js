9519376cb322aa720144acd9befcdc2973c9b890
Abhijit H Kulkarni <abhijit.kulkarni@juspay.in>
2020-03-12 01:29:26 +0530

    Initial Build

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Title from './components/Title'
+
+function App() {
+  return (
+    <div className="Title">
+        <Title/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Title.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+// for rendering title in App.js
+const getTitle = () => <h1>Akshaya Vipra Sangha</h1>
+
+export default getTitle
